04529425a13305d04b377affe07b247175775504
Robbie Kruszynski <robbiekruszynski@gmail.com>
2019-07-21 17:00:20 -0700

    finish work on contact page

--- src/components/Main.js
+++ src/components/Main.js
@@ -137,28 +137,13 @@ class Main extends React.Component {
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
-            </div>
-            <ul className="actions">
-              <li><input type="submit" value="Send Message" className="special" /></li>
-              <li><input type="reset" value="Reset" /></li>
-            </ul>
-          </form>
-          <ul className="icons">
 
-            <li><a href="#" className="icon"><span className="label">IG</span></a></li>
-            <li><a href="#" className="icon"><span className="label">GH</span></a></li>
+          <h3>robbiekruszynski@gmail.com</h3>
+          <a href="https://docs.google.com/presentation/d/1sL75Aqez-TWTaELnlZvwG2L_hYo4UMbG_qGcOKzX3D4/edit?usp=sharing"><h4>Digital Portfolio</h4></a>
+          <a href="https://www.linkedin.com/in/robbie-k/"><h4>Linkedin</h4></a>
+          <ul className="icons">
+            <li><a href="https://www.instagram.com/_wave_dash/" className="icon"><span className="label">IG</span></a></li>
+            <li><a href="https://github.com/Robbiekruszynski" className="icon"><span className="label">GH</span></a></li>
           </ul>
           {close}
         </article>
